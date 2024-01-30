07ba718da24d4091e24bc5dce73f7bca5ab95170
WhatIsLove-1 <kuzinitisou@mail.ru>
2024-01-30 12:26:53 +0300

    h

--- main.py
+++ main.py
@@ -16,4 +16,42 @@ df = pd.read_excel(
 )
 df_selection=(df.iloc[199:400])
 
+
+
+st.sidebar.header("Фильтр")
+city=st.sidebar.multiselect(
+    "Выберете город:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+customer_type = st.sidebar.multiselect(
+    "Выберете тип покупателя:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique()
+)
+
+gender=st.sidebar.multiselect(
+    "Выберете пол:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+branch=st.sidebar.multiselect(
+    "Выберете категорию:",
+    options=df["Branch"].unique(),
+    default=df["Branch"].unique()
+)
+
+product_line=st.sidebar.multiselect(
+    "Выберете линийку продуктов",
+    options=df["Product_line"].unique(),
+    default=df["Product_line"].unique()
+)
+
+df_selection = df.query(
+    "City == @city & Customer_type == @customer_type & Gender == @gender & Branch == @branch & Product_line == @product_line"
+)
+
 st.dataframe(df_selection)
+
