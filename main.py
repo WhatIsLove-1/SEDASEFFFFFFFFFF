b7369291677b92fd8ba3bd22a4dc2398eaa9848f
WhatIsLove-1 <kuzinitisou@mail.ru>
2024-01-31 21:17:05 +0300

    h

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ import plotly.express as px
 st.set_page_config(page_title="Excel Plotter")
 st.title("Excel Plotter 📈")
 st.subheader("Feed with your Excel file")
+
+uploaded_file = st.file_uploader("Выберете XLSX-файл", type="xlsx")
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+    st.dataframe(df)
